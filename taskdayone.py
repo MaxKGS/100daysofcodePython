da2d623d1cbc8317049aad3166c563ded9f389f1
Maxine Klein <87439325+MaxKGS@users.noreply.github.com>
2025-08-24 14:37:29 -0300

    Add files via upload

---
+++ taskdayone.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+firstName = input("What is the name of the city you grew up?\n")
+secondName = input("What is your pet's name?\n")
+print("Your band name should be: " + firstName + " " + secondName)
